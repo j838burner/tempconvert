305ff158b34311f1fca674fd2cc557b7f00ddaf8
j838burner <51219226+j838burner@users.noreply.github.com>
2019-05-30 21:57:41 -0600

    Update tempconvert.py
    Added Boys Life code as reference and started to define one function

--- tempconvert.py
+++ tempconvert.py
@@ -2,3 +2,37 @@
 # tempconvert.py - Convert temperature input
 #
 import math
+
+# ========================= Start Boys Life Code ===========================
+#
+#continueYN = "y"
+ 
+#while continueYN == "y":
+   #...get temperature input from the user
+#   sDegreeF = input("Enter next temperature in degrees Farenheight (F):")
+ 
+   #...convert text entry to number value that can be used in equations
+#   nDegreeF = int(sDegreeF)
+ 
+   #...convert temperature from F to Celsius
+#   nDegreeC = (nDegreeF - 32) * 5 / 9
+ 
+#   print ("Temperature in degrees C is:", nDegreeC)
+ 
+   #...check for temperature below freezing..
+#   if nDegreeC < 0:
+#      print ("Pack long underwear!")
+ 
+   #...check for it being a hot day...
+#   if nDegreeF > 100:
+#      print ("Remember to hydrate!")
+ 
+#   continueYN = input("Input another?")
+ 
+#exit the program
+# ========================= End Boys Life Code ===========================
+
+
+def pick_direction():
+  print("Enter F or C")
+  
